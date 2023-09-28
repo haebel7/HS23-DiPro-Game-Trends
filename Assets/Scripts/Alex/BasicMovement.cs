2e15816bdc09eda3476fbf37d630e28773fa96be
Alex Haeberlin <alex.haeberlin@stud.hslu.ch>
2023-09-28 13:46:16 +0200

    Started on isometric cam

---
+++ Assets/Scripts/Alex/BasicMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    private CharacterController charController;
+    [SerializeField]
+    private float Speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0 , Input.GetAxis("Vertical"));
+
+        charController.Move(move*Time.deltaTime*Speed);
+    }
+}
