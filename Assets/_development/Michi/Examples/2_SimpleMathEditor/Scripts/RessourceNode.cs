c678639f16ba24c3a782f48064d7fe2b014a05fe
Necromantic98 <91538012+Necromantic98@users.noreply.github.com>
2023-10-03 17:31:22 +0200

    crazy Ressource node und smelter node

--- Assets/_development/Michi/Examples/2_SimpleMathEditor/Scripts/RessourceNode.cs
+++ Assets/_development/Michi/Examples/2_SimpleMathEditor/Scripts/RessourceNode.cs
@@ -1,11 +1,143 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.Rendering.DebugUI;
+using Button = UnityEngine.UI.Button;
 
 namespace RuntimeNodeEditor.Examples
 {
+    public class Packet : MonoBehaviour
+    {
+        private int itemCount;
+        private string ressource;
+        //private int priority;
+
+        public void setAttributes(int itemCount, string ressource)
+        {
+            this.itemCount = itemCount;
+            this.ressource = ressource;
+            //this.priority = priority;
+        }
+
+        public int getItemCount() { return this.itemCount; }
+        public string getRessource() {  return this.ressource; }
+    }
+    
     public class RessourceNode : Node
     {
+        public TMP_InputField prio;
+        public SocketOutput outputSocketLeft;
+        public SocketOutput outputSocketRight;
+        public Button buttonAdd1;
+        public Button buttonAdd10;
+        public Button buttonAdd_1;
+        public Button buttonAdd_10;
+        public TMP_Dropdown dropdown;
+        private VisualElement root;
 
+        public override void Setup()
+        {
+            root = GameObject.Find("UIDocument").GetComponent<UIDocument>().rootVisualElement;
+            Register(outputSocketLeft);
+            Register(outputSocketRight);
+            SetHeader("Ressource");
+
+            prio.text = "0";
+            
+            buttonAdd1.onClick.AddListener(delegate { priority(int.Parse(buttonAdd1.GetComponentInChildren<TextMeshProUGUI>().text)); });
+            buttonAdd10.onClick.AddListener(delegate { priority(int.Parse(buttonAdd10.GetComponentInChildren<TextMeshProUGUI>().text)); });
+            buttonAdd_1.onClick.AddListener(delegate { priority(int.Parse(buttonAdd_1.GetComponentInChildren<TextMeshProUGUI>().text)); });
+            buttonAdd_10.onClick.AddListener(delegate { priority(int.Parse(buttonAdd_10.GetComponentInChildren<TextMeshProUGUI>().text)); });
+            
+            dropdown.AddOptions(new List<TMP_Dropdown.OptionData>()
+            {
+                new TMP_Dropdown.OptionData("Iron"),
+                new TMP_Dropdown.OptionData("Copper")
+            });
+
+            dropdown.onValueChanged.AddListener(selected =>
+            {
+                HandleFieldValue(getPriority().ToString());
+            });
+
+            prio.onEndEdit.AddListener(HandleFieldValue);
+
+            HandleFieldValue(prio.text);
+        }
+
+        private void HandleFieldValue(string prio)
+        {
+            string ressource = getRessource();
+            int ressourceCount = getRessourceCount(ressource);
+
+            Packet packet = new Packet();
+            packet.setAttributes(ressourceCount, ressource);
+            
+            outputSocketLeft.SetValue(packet);
+            outputSocketRight.SetValue(packet);
+        }
+
+        private void priority(int number)
+        {
+            int countNow = int.Parse(prio.text);
+            int countNew = countNow + number;
+            prio.text = countNew.ToString();
+            HandleFieldValue(countNew.ToString());
+        }
+
+        public int getPriority()
+        {
+            return int.Parse(prio.text);
+        }
+
+        public string getRessource()
+        {
+            return dropdown.options[dropdown.value].text;
+        }
+
+        public int getRessourceCount(string ressource)
+        {
+            switch(ressource)
+            {
+                case "Iron": return root.Q<IntegerField>("ironCount").value;
+                case "Copper": return root.Q<IntegerField>("copperCount").value;
+                default: return -1;
+            }
+        }
+
+        public int setRessourceCount(string ressource, int amount)
+        {
+            if (getRessourceCount(ressource) >= amount)
+            {
+                switch (ressource)
+                {
+                    case "Iron": return root.Q<IntegerField>("ironCount").value -= amount;
+                    case "Copper": return root.Q<IntegerField>("copperCount").value -= amount;
+                    default: return -1;
+                }
+            } 
+            else
+            {
+                return -1;
+            }
+        }
+
+        public override void OnSerialize(Serializer serializer)
+        {
+            serializer.Add("ressource", getRessourceCount(getRessource()).ToString());
+        }
+
+        public override void OnDeserialize(Serializer serializer)
+        {
+            var value = serializer.Get("ressource");
+            prio.SetTextWithoutNotify(value);
+
+            HandleFieldValue(value);
+        }
     }
 }
--- Assets/giveRessources.cs
+++ Assets/giveRessources.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class giveRessources : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private VisualElement root;
+    public VisualTreeAsset vta;
+    public IntegerField ironCount;
+    public IntegerField copperCount;
+    public Button giveIron;
+    public Button giveCopper;
+    private void OnEnable()
     {
-        
+        root = GetComponent<UIDocument>().rootVisualElement;
+        giveIron = root.Q<Button>("giveIron");
+        giveCopper = root.Q<Button>("giveCopper");
+        ironCount = root.Q<IntegerField>("ironCount");
+        copperCount = root.Q<IntegerField>("copperCount");
+
+        giveIron.clicked += giveIronButtonPressed;
+        giveCopper.clicked += giveCopperButtonPressed;
     }
 
-    // Update is called once per frame
-    void Update()
+    void giveIronButtonPressed()
     {
-        
+        ironCount.value += 50;
+    }
+
+    void giveCopperButtonPressed()
+    {
+        copperCount.value += 50;
+    }
+
+    public int getIronCount()
+    {
+        return ironCount.value;
+    }
+
+    public int getCopperCount()
+    {
+        return copperCount.value;
+    }
+
+    public void decrementIronCount(int number)
+    {
+        ironCount.value -= number;
+    }
+
+    public void decrementCopperCount(int number)
+    {
+        copperCount.value -= number;
     }
 }
