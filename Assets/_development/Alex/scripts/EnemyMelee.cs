538db5dd4273f2e3e23ee2fccb46b2fcc116b18b
Alex Haeberlin <alex.haeberlin@stud.hslu.ch>
2023-10-05 11:35:05 +0200

    Deleted unused ContextSteering

--- Assets/_development/Alex/scripts/EnemyMelee.cs
+++ Assets/_development/Alex/scripts/EnemyMelee.cs
@@ -8,17 +8,15 @@ public class EnemyMelee : MonoBehaviour
     [SerializeField]
     private Transform player;
 
-    private NavMeshAgent agent;
-
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = player.position;
+        
     }
 }
