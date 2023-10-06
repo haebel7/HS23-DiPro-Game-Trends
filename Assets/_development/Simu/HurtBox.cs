cf06386b4926584f322f69ca8870650caf7fa8c7
simitomorrow <email@michelsimon.ch>
2023-10-06 13:25:41 +0200

    DamageType

--- Assets/_development/Simu/HurtBox.cs
+++ Assets/_development/Simu/HurtBox.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 public class HurtBox : MonoBehaviour
 {
     public HealthObject health;
+    public List<DamageType> canBeHitBy;
 
-    public void TakeDamage(int damageAmount)
+    public void TakeDamage(int damageAmount, DamageType damageType)
     {
-        health.currentHealth -= damageAmount;
-        CheckDied();
+        Debug.Log(canBeHitBy[0]);
+        Debug.Log(damageType);
+        if (canBeHitBy.Contains(damageType))
+        {
+            health.currentHealth -= damageAmount;
+            CheckDied();
+        }
     }
 
     public void CheckDied()
