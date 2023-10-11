8eb273b87728ffd347a8d4b7f1b51e2a3d1abf60
Alex Haeberlin <alex.haeberlin@stud.hslu.ch>
2023-10-11 10:44:06 +0200

    Enemy Melee new state system

--- Assets/_development/Alex/scripts/EnemyMelee.cs
+++ Assets/_development/Alex/scripts/EnemyMelee.cs
@@ -24,35 +24,78 @@ public class EnemyMelee : MonoBehaviour
     private float attackDistance = 2f;
 
     // Additional Behaviours
-    private bool bPlayerInRange = false;
-    private bool bLowHealth = false;
+    //private bool bPlayerInRange = false;
+    //private bool bLowHealth = false;
 
     private Animator anim;
+    //private HurtBox hurtBox;
+
+    private EnemySState lastState;
 
     // Start is called before the first frame update
     void Start()
     {
         state = EnemySState.HUNT;
+        lastState = state;
         anim = GetComponent<Animator>();
+        //hurtBox = GetComponent<HurtBox>();
     }
 
     void FixedUpdate()
     {
-        if (state == EnemySState.HUNT && Vector3.Distance(transform.position, player.position) < attackDistance)
+        if (/*state == EnemySState.HUNT &&*/ Vector3.Distance(transform.position, player.position) < attackDistance)
         {
             state = EnemySState.ATTACK;
-            anim.SetTrigger("Attacking");
-            GetComponent<PlanarMovement>().SetIsMoving(false);
         }
-        else if (state == EnemySState.ATTACK && anim.GetCurrentAnimatorClipInfo(0)[0].clip.name != "EnemySAttack")
-        {
-            state = EnemySState.HUNT;
-            GetComponent<PlanarMovement>().SetIsMoving(true);
-        }
+
+        CheckEnemyState();
     }
 
-    private void checkEnemyState()
+    public void LeaveAttackState()
     {
+        state = EnemySState.HUNT;
+        CheckEnemyState();
+    }
 
+    private void CheckEnemyState()
+    {
+        if (lastState != state)
+        {
+            // Cleanup last state behaviour
+            switch (lastState)
+            {
+                case EnemySState.IDLE:
+                    // stop idle anim
+                    break;
+                case EnemySState.HUNT:
+                    anim.SetBool("Hunt", false);
+                    GetComponent<PlanarMovement>().SetIsMoving(false);
+                    break;
+                case EnemySState.ATTACK:
+                    anim.SetBool("Attack", false);
+                    break;
+                default:
+                    break;
+            }
+
+            // Trigger any behaviours of the new state
+            switch (state)
+            {
+                case EnemySState.IDLE:
+                    // start idle anim
+                    break;
+                case EnemySState.HUNT:
+                    anim.SetBool("Hunt", true);
+                    GetComponent<PlanarMovement>().SetIsMoving(true);
+                    break;
+                case EnemySState.ATTACK:
+                    anim.SetBool("Attack", true);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        lastState = state;
     }
 }
