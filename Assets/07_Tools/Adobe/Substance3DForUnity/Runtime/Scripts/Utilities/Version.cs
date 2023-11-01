5d7de6d3a132deddc0477f57dcda190161dbb97a
silvanphilipp <91538160+silvanphilipp@users.noreply.github.com>
2023-11-01 14:38:16 +0100

    Removed Adobe Folder
    Adobe Folder

--- Assets/07_Tools/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/Version.cs
+++
@@ -1,9 +0,0 @@
-namespace Adobe.Substance
-{
-    public static class Version
-    {
-        public const string PluginVersion = "3.9.0-7-ged9331f";
-        public const string EngineVersion = "9.0.0 537e2e31";
-        public const int ImporterVersion = 390;
-    }
-}
